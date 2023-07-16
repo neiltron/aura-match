b827460dd395d00814048a404c02232fd740a835
Neil Pullman <npullman@thisisgrow.com>
2023-07-15 22:32:21 -0400

    write about copy, clean up styles

--- src/script.js
+++ src/script.js
@@ -3,12 +3,16 @@ const { ImageEmbedder, FilesetResolver } = vision;
 import ids from './files.json'
 
 let nodes = [];
+let els = [];
+let rotationInterval;
+let imgIndex;
+let imageEmbedder;
 
 const loadModels = async () => {
     const vision = await FilesetResolver.forVisionTasks(
         "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
     );
-    const imageEmbedder = await ImageEmbedder.createFromOptions(
+    imageEmbedder = await ImageEmbedder.createFromOptions(
         vision,
         {
             baseOptions: {
@@ -17,50 +21,6 @@ const loadModels = async () => {
         }
     );
 
-    let els = [];
-
-    const processImage = (id) => {
-        const el = document.querySelector('#canvasDiv');
-        const image = new Image();
-        image.src = `/photos/5k-compressed/5k-compressed/${id}`;
-
-        els[0].style.backgroundImage = `url(/photos/5k-compressed/5k-compressed/${id})`;
-        
-        image.onload = async () => {
-            els[0].className = 'active';
-
-            const imageEmbedderResult = imageEmbedder.embed(image);
-            const embedding = imageEmbedderResult.embeddings[0]
-
-            const response = await fetch('/api/text/', {
-                method: 'post',
-                mode: 'cors',
-                body: JSON.stringify(embedding.floatEmbedding),
-                headers: {
-                    'Accept': 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                },
-            })
-
-            let distances;
-
-            try {
-                const resp = await response.json();
-                const { distances } = resp;
-
-                distances.slice(0, 7).forEach((distance, i) => {
-                els[i + 1].style.backgroundImage = `url(/photos/5k-compressed/5k-compressed/${distance.name})`;
-
-                setTimeout(() => {
-                    els[i + 1].className = 'active';
-                }, 40 * i);
-            })
-            } catch (e) {
-                console.log(e);
-            }
-        }
-    };
-
     const container = document.querySelector('#canvasDiv');
     for (let i = 0; i < 8; i += 1) {
         const div = document.createElement('div');
@@ -68,10 +28,56 @@ const loadModels = async () => {
         els.push(div);
     }
 
-    let imgIndex = Math.floor(Math.random() * ids.length);
+    imgIndex = Math.floor(Math.random() * ids.length);
     processImage(ids[imgIndex]);
 
-    setInterval(() => {
+    startInterval();
+};
+
+const processImage = (id) => {
+    const el = document.querySelector('#canvasDiv');
+    const image = new Image();
+    image.src = `/photos/5k-compressed/5k-compressed/${id}`;
+
+    els[0].style.backgroundImage = `url(/photos/5k-compressed/5k-compressed/${id})`;
+    
+    image.onload = async () => {
+        els[0].className = 'active';
+
+        const imageEmbedderResult = imageEmbedder.embed(image);
+        const embedding = imageEmbedderResult.embeddings[0]
+
+        const response = await fetch('/api/text/', {
+            method: 'post',
+            mode: 'cors',
+            body: JSON.stringify(embedding.floatEmbedding),
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+        })
+
+        let distances;
+
+        try {
+            const resp = await response.json();
+            const { distances } = resp;
+
+            distances.slice(0, 7).forEach((distance, i) => {
+            els[i + 1].style.backgroundImage = `url(/photos/5k-compressed/5k-compressed/${distance.name})`;
+
+            setTimeout(() => {
+                els[i + 1].className = 'active';
+            }, 40 * i);
+        })
+        } catch (e) {
+            console.log(e);
+        }
+    }
+};
+
+const startInterval = () => {
+    rotationInterval = setInterval(() => {
         els.forEach((el, i) => {
             setTimeout(() => {
                 el.className = '';
@@ -84,15 +90,34 @@ const loadModels = async () => {
             imgIndex = Math.floor(Math.random() * ids.length);
             processImage(ids[imgIndex]);
         }, 250);
-    }, 6000)
+    }, 6000);
 };
+
+const stopInterval = () => {
+    clearInterval(rotationInterval);
+};
+
+document.addEventListener('visibilitychange', () => {
+    if (document.visibilityState === 'visible') {
+        startInterval();
+    } else {
+        stopInterval();
+    }
+});
 
 loadModels();
 
 document.querySelector('#que-es').addEventListener('click', () => {
-    document.querySelector('dialog')['showModal']();
+    document.querySelector('dialog')['show']();
+    stopInterval();
+});
+
+document.querySelector('#okay').addEventListener('click', () => {
+    document.querySelector('dialog')['close']();
+    startInterval();
 });
 
 document.querySelector('#close-modal').addEventListener('click', () => {
     document.querySelector('dialog')['close']();
+    startInterval();
 });
